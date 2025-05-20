b46c2bf31eccd3e8c2db8b98d1fc94e8995bdaab
p-reetu <manchekar.preeti@gmail.com>
2025-05-20 08:39:26 +0530

    Connected  to Postgresql

--- InvestInsight/InvestInsight/settings.py
+++ InvestInsight/InvestInsight/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'InvestInsight.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'investinsight',
+        'HOST' : '127.0.0.1',
+        'PORT' : '5432',
+        'USER' : 'admin',
+        'PASSWORD' : 'secret',
     }
 }
 
